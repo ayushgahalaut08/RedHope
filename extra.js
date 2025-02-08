89cf1dfa684de5e9065433af951049e4ecf38cc8
Ayush Gahalaut <ayushgahalaut08@gmail.com>
2025-02-08 23:40:18 +0530

    8 feb

--- extra.js
+++ extra.js
@@ -14,3 +14,30 @@ window.onscroll=()=>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+var swiper = new Swiper(".reviews-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoHeight: true,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
